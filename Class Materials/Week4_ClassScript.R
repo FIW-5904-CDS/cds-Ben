f5ec97c5eee0958bf6dd97e7a7173b3682c0a045
Ben Stalheim <benstalheim@gmail.com>
2025-09-17 15:20:24 -0400

    Adding Class Materials

---
+++ Class Materials/Week4_ClassScript.R
@@ -0,0 +1,20 @@
+# Author: Ben Stalheim
+# Date: September 17, 2025
+###########################################
+
+#### Required Packages ----
+
+#### Setting Working Directory ---- 
+rm(list = ls())
+
+setwd("C:/cygwin64/home/benst/cds-Ben/BACS_Sound")
+
+#### Playing Around ----
+abc <- c("a", "b", "c")
+
+x <- c(3:8)
+z <- c(8, 13, 14, 19, 24)
+y <- z -x
+
+my_birthday <- 7 + 2 +2 +1+1+9+7 
+
